e31f804c588b54df07cab3ebdaa5c972fe9909b2
Sibgatullah <sibgatullah881@gmail.com>
2025-10-24 22:17:37 +0600

    find method

---
+++ PYTHON STRING OPERATIONS/5-find method.py
@@ -0,0 +1,5 @@
+# find() method is search for the specific letter in the string
+str = "this is a dummy text"
+print(str.find('text'))
+# The answer will be 16 as the text word is found on the 16 index 
+# we can also put starting and ending value like range() function .find(value, start, end)
