fff8485d9382f7b847389a5bf44ceffa9315ab09
Sibgatullah <sibgatullah881@gmail.com>
2025-10-24 22:14:08 +0600

    count method

---
+++ PYTHON STRING OPERATIONS/4-count method.py
@@ -0,0 +1,5 @@
+# count() method searches a specific value and counts how many times that value has appeared in the string 
+
+str = 'This is a demo. This is another demo.'
+print(str.count('is'))
+# we can also put start and end value in here like a loop count(value, start index, end index)
