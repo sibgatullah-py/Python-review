274794f95bc9c49206c17e1fc550f8bc659b528d
Sibgatullah <sibgatullah881@gmail.com>
2025-10-24 22:21:09 +0600

    endswith() method

---
+++ PYTHON STRING OPERATIONS/6-endswith method.py
@@ -0,0 +1,4 @@
+# endswith() method returns True or False based on the position of a specific value at the end of a string
+str = 'Bangladesh'
+print(str.endswith('desh'))
+# we can also put starting and ending value like range() function .find(value, start, end)
