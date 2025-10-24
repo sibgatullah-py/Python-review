2a6d785fce716a99c9492a795a2af7c039d0332a
Sibgatullah <sibgatullah881@gmail.com>
2025-10-24 22:26:56 +0600

    index() method

---
+++ PYTHON STRING OPERATIONS/8-index method.py
@@ -0,0 +1,5 @@
+# index() method is used to find the first occurrence of a specific value
+
+str = 'This is a demo data'
+print(str.index('a'))
+# we can also put starting and ending value like range() function .find(value, start, end)
