0de163dab97fed90883147a188806cb5e8b706d6
Sibgatullah <sibgatullah881@gmail.com>
2025-10-26 19:13:03 +0600

    istitle() method

---
+++ PYTHON STRING OPERATIONS/18-istitle method.py
@@ -0,0 +1,5 @@
+# istitle() method returns true if the string fills the rules of a title which is capitalize
+str = "Hello" # True
+str2 = 'THIS' # False
+print(str.istitle())
+print(str2.istitle())
