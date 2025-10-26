be97e83125aa5a661e1ceb083e62bcd2a4d5db95
Sibgatullah <sibgatullah881@gmail.com>
2025-10-26 19:00:44 +0600

    isidentifier() method

---
+++ PYTHON STRING OPERATIONS/13-isidentifier method.py
@@ -0,0 +1,7 @@
+# isidentifier() method returns True if the string is an identifier 
+# an identifier starts with a-z 1-9 and _ . it won't include any special characters or space
+
+str = 'demo1234'
+str2= '@bs sdf'
+print(str.isidentifier())
+print(str2.isidentifier())
