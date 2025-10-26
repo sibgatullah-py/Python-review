ef5829bc444c2614aee50b4cb0e5cdeb3fdf4646
Sibgatullah <sibgatullah881@gmail.com>
2025-10-26 19:31:52 +0600

    replace() method

---
+++ PYTHON STRING OPERATIONS/25-replace method.py
@@ -0,0 +1,5 @@
+# replace() method is used to replace a specific value into a different value 
+# str.replace('replacing','replaced with',how many of them will be replaced)
+
+str = 'this is a demo . this is another demo and demo for the third time.'
+print(str.replace('demo','text',2))# the first 2 demos will be replaced but the 3rd won't cause we said 2 demos will be replaced 
