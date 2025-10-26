6c94b0b299466fca33e7e7856380305408a50942
Sibgatullah <sibgatullah881@gmail.com>
2025-10-26 19:25:53 +0600

    rstrip() method

---
+++ PYTHON STRING OPERATIONS/23-rstrip method.py
@@ -0,0 +1,3 @@
+# rstrip() method is used to remove a specific character from the right of the string
+str = "bangladesh#####"
+print(str.rstrip('#')) # if we don't define which character needs to be removed it removes space as default
