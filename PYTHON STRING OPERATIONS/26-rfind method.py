c06e46b2b70f63ebdee72b2cdd7defff799e2c86
Sibgatullah <sibgatullah881@gmail.com>
2025-11-08 20:43:35 +0600

    rfind method

---
+++ PYTHON STRING OPERATIONS/26-rfind method.py
@@ -0,0 +1,4 @@
+# rfind() method is used to find the last occarance of a value in a string
+
+str = "this is demo, this is another demo" # This will return where the last demo is placed
+print(str.rfind("demo"))
