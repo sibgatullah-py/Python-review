64c3f23d264f512524a96fd5767b37128741fe5a
Sibgatullah <sibgatullah881@gmail.com>
2025-11-08 20:45:49 +0600

    rindex() method same as rfind()

---
+++ PYTHON STRING OPERATIONS/27-rindex method.py
@@ -0,0 +1,3 @@
+# rindex() method is used to get the index of the last occarence of a spacific value kinda same as rfind() method
+str = " this is a demo again another demo also a demo"
+print(str.rindex('demo'))
