62f71d24e1b956f1cb6a1cf1e933f61a2feb37fa
Sibgatullah <sibgatullah881@gmail.com>
2025-11-08 22:08:54 +0600

    some use of startswith() method

--- PYTHON STRING OPERATIONS/31-startswitch method.py
+++ PYTHON STRING OPERATIONS/31-startswitch method.py
@@ -1,3 +1,3 @@
 # startswitch() method returns True if string starts with a particuler value or returns false 
 str = "This is a demo"
-print(str.startswith('This'))
+print(str.startswith('This'))# we can also set the begining and end value ("This",2,6)
